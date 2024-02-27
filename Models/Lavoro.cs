645a65f5f0edf5ce91bc8beffddad33dc0aac984
Francesco <sr1163374794@live.com>
2024-02-27 17:02:34 +0100

    Aggiunto lavoro

---
+++ Models/Lavoro.cs
@@ -0,0 +1,17 @@
+﻿namespace Edilizia_v2.Models
+{
+    public class Lavoro
+    {
+      
+        
+            public int LavoroId { get; set; }
+            public string Periodo { get; set; }
+            public int Pagamento { get; set; }
+            public string  Tipo{ get; set; }
+     
+
+        }
+    }
+
+
+
